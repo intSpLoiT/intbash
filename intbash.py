04e27124295b7b10ab2a4e5a25a390ddc233f379
İNTİKAM21 CYBER <167871296+Intikam21kurucu@users.noreply.github.com>
2024-06-01 17:26:54 +0300

    Update intbash.py

--- intbash.py
+++ intbash.py
@@ -1,6 +1,18 @@
 import os
+import re
+from termcolor import colored
+
+# Kullanıcının metin rengini sadece bir kez girmesini sağlamak için bir değişken
+user_text_color_set = False
 
 def print_banner():
+    try:
+        import pyfiglet
+        import termcolor
+    except ImportError:
+        print("Please install 'pyfiglet' and 'termcolor' packages to use this script.")
+        return
+    
     banner_art = """
 ⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⠶⠾⠿⠛⠛⠻⠿⠶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢠⣾⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣷⣄⠀⠀⠀⠀⠀
@@ -14,9 +26,9 @@ def print_banner():
 ⠻⠷⠶⠶⣶⣤⣈⠙⠻⣿⣷⣦⠀⠸⠋⠙⠟⠀⣠⣾⣿⠟⠋⣁⣠⣴⠶⠶⠟
 ⠀⠀⠀⠀⠀⠉⠛⠿⣶⣼⠿⣿⣲⡤⡤⡤⢤⢰⣿⡏⣿⣶⠿⠛⠉⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⡄⠻⣹⡟⡟⡟⣻⣻⠽⠁⣿⣦⣄⡀⠀⠀⠀⠀⠀
-⠀⠀⣶⠾⠶⠾⠟⠋⣁⣼⣷⡀⠀⠉⠉⠉⠉⠀⢀⣼⣧⣀⠉⠛⠷⠶⠿⣶⠀
-⠀⠀⠙⣷⡄⢀⣴⠿⠛⠁⠀⠙⠳⠶⠤⠴⠶⠞⠋⠀⠈⠙⠻⣶⡄⠀⣾⠟⠀
-⠀⠀⠀⢸⣷⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣶⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⣶⠾⠶⠾⠟⠋⣁⣼⣷⡀⠀⠉⠉⠉⠉⠀⢀⣼⣧⣀⠉⠛⠷⠶⠿⣶⠀
+⠀⠀⠀⠀⠀⠙⣷⡄⢀⣴⠿⠛⠁⠀⠙⠳⠶⠤⠴⠶⠞⠋⠀⠈⠙⠻⣶⡄⠀⣾⠟⠀
+⠀⠀⠀⠀⠀⢸⣷⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣶⡿⠀⠀⠀
 -----------------------------------------------------------
 
 ___ _   _ _____ ___ _  __    _    __  __ ____  _ _ ____
@@ -25,12 +37,31 @@ ___ _   _ _____ ___ _  __    _    __  __ ____  _ _ ____
  | || |\\  | | |  | || . \\  / ___ \\| |  | |/ __/| |  ___) |
 |___|_| \\_| |_| |___|_|\\_\\/_/   \\_\\_|  |_|_____|_| |____/
 """
+
+    global user_text_color_set
     current_directory = os.getcwd()
-    prompt = f"┌──(intikam21-cyber@root[{current_directory}]
-  └─$ "
-    print(banner_art)
+    
+    # Kullanıcı metin rengini henüz belirlemediyse sormak için
+    if not user_text_color_set:
+        user_text_color = input("Enter the color for user text (e.g., red, green, blue): ")
+        user_text_color_set = True
+
+    # Metin rengini sadece bir kez ayarla
+    try:
+        user_input_color = getattr(termcolor, user_text_color.lower())
+    except AttributeError:
+        print("Invalid color. Using default color.")
+        user_input_color = "white"
+
+    # Kullanıcı metni renkli yazdır
+    print(colored(banner_art, user_input_color))
+
+    prompt = f"┌──(intikam21-cyber@root[{colored(current_directory, 'cyan')}])\n  └─$ "
     user_text = input(prompt)
-    os.system(f"echo {user_text} | figlet | lolcat")
+
+    # Eğer kullanıcı komutu, bir harf eksik veya fazlaysa çalıştırma
+    if re.search(r'^ls$', user_text):
+        os.system(f"echo {user_text} | figlet | lolcat")
 
 if __name__ == "__main__":
     print_banner()
